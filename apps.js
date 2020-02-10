025c85d4467083a66c9baeba11da10ee6fbb89db
Tremain-Ajena Jones <tremainajena.j@gmail.com>
2020-02-10 14:12:04 -0600

    still not finished but further than before

--- apps.js
+++ apps.js
@@ -1,27 +1,65 @@
 let button1 = $('#make-die');
 let button2 = $('#roll-die');
+let button3 = $('#sum-die');
 class Die {
     constructor(value) {
         this.value = value;
     }
     roll() {
         // should generate a random integer 1-6
-        Math.floor(Math.random() * 6);
         // should set the 'value' property
         // should update the div on the screen w/ the new value
+
+        let nextNumber = Math.floor(Math.random() * 7);
+        let dieID = nextNumber;
+        // diceClick.remove();
+        diceClick.append(dieID);
+        console.log('It is me!' + dieID);
     }
 };
+
+button2.click(function (event) {
+    let nextNumber = Math.floor(Math.random() * 7);
+    if (dieID == w) {
+        // let newValue = nextNumber;
+        // diceClick.remove();
+        // diceClick.append(dieID);
+        diceClick.text(nextNumber);
+        dieHolder.append(diceClick);
+        $('body').append(nextNumber);
+
+        console.log('It is me!' + dieID);
+    };
+})
 
 button1.click(function (event) {
     let w = Math.floor(Math.random() * 7);
     console.log(w);
-    let dieClick = $('<div class="square">');
-    // dieClick.className = 'square'
-    let dieHolder = $('<div id="dieHolder">');
+    let diceClick = $('<div class="square" id="dice">');
     let dieID = w;
-    dieHolder.append(dieClick);
+    diceClick.append(dieID);
+    dieHolder.append(diceClick);
     $('body').append(dieHolder);
-    dieClick.append(dieID);
-    dieClick.id = w;
-    // i++;
+
+    dieHolder.dblclick(function () {
+        diceClick.remove();
+    });
+});
+
+
+let diceClick = $('<div class="square" id="dice">');
+let dieHolder = $('<div id="dieHolder">');
+let w = Math.floor(Math.random() * 7);
+let dieID = w;
+
+dieHolder.click(function (e) {
+    let nextNumber = Math.floor(Math.random() * 7);
+    if (dieID == w) {
+        // let dieID = nextNumber;
+        // diceClick.remove();
+        diceClick.text(nextNumber);
+        dieHolder.append(diceClick);
+        // $('body').append(nextNumber);
+        console.log('It is me!' + nextNumber);
+    };
 });
